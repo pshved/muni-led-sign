bc38009b2e7d9d906b3b62f67d3b04e788f06613
P. Shved <pavel.shved@gmail.com>
2013-06-24 00:05:45 -0700

    Add font reader and a simple font

---
+++ client/font/f.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+# In order to not introduce extra dependencies such as Freetype O_o, I just
+# convert the font into simpler and more editable format.  Besides, I add some
+# of my own glyphs.
+#
+# <code> <horizontal shift> <vertical shift> ...
+# 101
+# 110
+# 111
+#  <---- empty line
+#
+#
+# Shifts are from baseline (not from top).
+
+use Font::FreeType;
+my $freetype = Font::FreeType->new;
+# I have no idea how this stuff works, but this font and size 10 give 7x7
+# bitmaps!
+my $face = $freetype->face('RepetitionScrolling.ttf');
+my $sz = 10;
+$face->set_pixel_size($sz, $sz);
+
+# $face->set_char_size($sz, $sz, 100, 100);
+
+for my $c (1..127) {
+  my $glyph = $face->glyph_from_char_code($c);
+  # Do not print the unprintable.
+  next unless $glyph;
+  my ($bitmap, $left, $top) = $glyph->bitmap(FT_RENDER_MODE_MONO);
+  $,=' ';
+  # Do not print characters that are invisible.
+  my $packed = ($c >= 32) ? chr($c) : '';
+  print $c, $left, ($sz-$top), $packed,  "\n";
+  for my $line (@$bitmap) {
+    # This is silly, but I don't know a better way to convert a binary string to
+    # 0s and 1s.
+    my $byte_line = unpack('H*', $line);
+    $byte_line =~ s/ff/#/g;
+    $byte_line =~ s/00/./g;
+    print $byte_line, "\n";
+  }
+  print "\n";
+}
+
