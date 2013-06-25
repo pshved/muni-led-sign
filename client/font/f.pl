38728fd91b2fbee9717fcc0dc3a2a5025a145051
P. Shved <pavel.shved@gmail.com>
2013-06-25 00:11:30 -0700

    Render messages with the custom font.

--- client/font/f.pl
+++ client/font/f.pl
@@ -10,7 +10,7 @@
 #  <---- empty line
 #
 #
-# Shifts are from baseline (not from top).
+# Shifts are coords of top left corner as viewed from baseline (not from top).
 
 use Font::FreeType;
 my $freetype = Font::FreeType->new;
@@ -28,17 +28,18 @@ for my $c (1..127) {
   next unless $glyph;
   my ($bitmap, $left, $top) = $glyph->bitmap(FT_RENDER_MODE_MONO);
   $,=' ';
+  $\="\n";
   # Do not print characters that are invisible.
   my $packed = ($c >= 32) ? chr($c) : '';
-  print $c, $left, ($sz-$top), $packed,  "\n";
+  print $c, $left, $top, $packed;
   for my $line (@$bitmap) {
     # This is silly, but I don't know a better way to convert a binary string to
     # 0s and 1s.
     my $byte_line = unpack('H*', $line);
-    $byte_line =~ s/ff/#/g;
-    $byte_line =~ s/00/./g;
-    print $byte_line, "\n";
+    $byte_line =~ s/ff/1/g;
+    $byte_line =~ s/00/0/g;
+    print $byte_line;
   }
-  print "\n";
+  print;
 }
 
