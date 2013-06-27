619ed0861dc47593200fb274a0095d7d59a766bc
P. Shved <pavel.shved@gmail.com>
2013-06-26 23:54:01 -0700

    Allow to supply many messages

--- client/lowlevel.pl
+++ client/lowlevel.pl
@@ -1,6 +1,12 @@
 #!/usr/bin/perl -w
 # This script simply udpates the sign with more data.  If you need to write a
 # more sophisticated client, just call this file from somewhere else.
+# 
+# Usage:
+# ./lowlevel.pl --type=text
+# Then, supply messages as stdin, in form of pictures comprized out of 0s and 1s
+# for --type=pic, or as text for --type=text.  Separate messaages by double
+# lines.
 
 use strict qw(subs vars);
 use warnings;
@@ -9,52 +15,55 @@ use Device::MiniLED;
 my $sign=Device::MiniLED->new(devicetype => "sign");
 
 my $type = 'text';
+my $number = undef;
 
 my $options_result = GetOptions(
   'type=s' => \$type,
 );
 
-if ($type eq 'pic') {
-#   my $width = 96;
-#   my $height = 16;
-#   my $data = '';
-#   for (my $i = 0; $i < $width * $height; ++$i) {
-#     $data .= (rand(2) > 1) ? '0' : '1';
-#   }
-#   print $data, "\n";
-  my $width = 0;
-  my $height = 0;
-  my $data = '';
-  for (<STDIN>) {
-    chomp;
-    next unless $_;
+my $height = 0;
+my $data = '';
+my @messages = ();
+while (<STDIN>) {
+  chomp;
+  if ($_) {
     $height ++;
     $data .= $_;
   }
-  # Remove wrong pixels
-  $data =~ s/[^01]//g;
-  $width = int(length($data)/$height);
-  # This will verify if we have correct number of bits.
-  my $pic = $sign->addPix(
-    height => $height,
-    width => $width,
-    data => $data,
-  );
-  $sign->addMsg(
-    data => $pic,
-    effect => 'hold',
-    speed => 5,
-  );
-} elsif ($type eq 'text') {
-  my @lines = (<STDIN>);
-  my $data = $lines[0];
-  chomp $data;
+  if (!$_ or eof(STDIN)) {
+    # Add message if we have some.
+    push @messages, {data => $data, height => $height} if ($data);
+    $height = 0;
+    $data = '';
+  }
+}
 
-  $sign->addMsg(
-    data => $data,
-    effect => (length($data) > 13) ? 'scroll' : 'hold',
-    speed => 1,
-  );
+for my $message_data (@messages) {
+  if ($type eq 'pic') {
+    my $data = $message_data->{data};
+    my $height = $message_data->{height};
+    $data =~ s/[^01]//g;
+    my $width = int(length($data)/$height);
+    # This will verify if we have correct number of bits.
+    my $pic = $sign->addPix(
+      height => $height,
+      width => $width,
+      data => $data,
+    );
+    $sign->addMsg(
+      data => $pic,
+      effect => 'hold',
+      # For multiple messages, the speed seems to control transition speed in
+      # multi-message mode.
+      speed => 1,
+    );
+  } else {
+    $sign->addMsg(
+      data => $_,
+      effect => (length($_) > 13) ? 'scroll' : 'hold',
+      speed => 1,
+    );
+  }
 }
 
 # #
